a06a3b89757a77707e97415903b95e497356a51d
subinmbr <subinmbr4@gmail.com>
2023-11-20 13:32:56 +0000

    Initial project

---
+++ src/test/java/testPackage/testClass.java
@@ -0,0 +1,26 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
+		
+		driver.close();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
